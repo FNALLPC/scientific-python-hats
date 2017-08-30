10508a5895bfe9526a42d7cbd05e4b6809d78c3c
Marguerite <mtonjes@gmail.com>
2017-08-30 16:54:45 -0500

    updated for cmslpc
    Updated following
    http://uscms.org/uscms_at_work/physics/computing/setup/condor_worker_nod
    e.shtml#CMSSW to do everything locally on the cmslpc condor worker
    node, rather than the previous brux cluster configuration. Be sure to
    change your input file directory location as appropriate for your
    analysis and username.

--- pyROOT_essentials/sample_code/condorSubmission/h_QCD_HT700to1000_0.sh
+++ pyROOT_essentials/sample_code/condorSubmission/h_QCD_HT700to1000_0.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd /my/cmssw/directory/CMSSW_9_x_x/src
+cd ${_CONDOR_SCRATCH_DIR}
+export SCRAM_ARCH=slc6_amd64_gcc530
+# make a brand new empty CMSSW working area
+eval `scramv1 project CMSSW CMSSW_9_2_8`
+cd CMSSW_9_2_8/src
 eval `scramv1 runtime -sh`
-cd /my/exercises/directory/pyROOT_essentials/sample_code
+echo "CMSSW: "$CMSSW_BASE
+cd ${_CONDOR_SCRATCH_DIR}
 python runHatsTrees.py -i QCD_HT700to1000_0_${1} -o QCD_HT700to1000_0_${1} -l -t 'ntuplizer/tree'
+# examples here: http://uscms.org/uscms_at_work/physics/computing/setup/condor_worker_node.shtml
